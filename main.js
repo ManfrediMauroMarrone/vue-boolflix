12fc0ffa40bc2e36d95b4492140f47a156fb6c22
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-11-25 23:41:35 +0100

    aggiunto icone e risultati serie tv

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var app = new Vue({
     el: '#root',
     data: {
       movies: [],
-      vote:'',
+      tvSeries: [],
       searched: '',
       results: '',
 
@@ -26,13 +26,30 @@ var app = new Vue({
             // pongo il vecchio voto in base 10 uguale al nuovo voto in pase 5 specificando l'indice perché è un array di oggetti
             this.movies[index].vote_average = votoBase5;
 
-            if (item.original_language == en) {
-              item.original_language = 'icons/en.png'
-            }
+          });
+          // this.movies = [];
+
+        })
+        .catch((error) =>{
+          console.log(error);
+        });
+
+        axios
+        .get('https://api.themoviedb.org/3/search/tv' ,{
+          params:{
+            api_key: '469d777c4caf641af43971c56a9b9340',
+            query: this.searched
+          }
+        })
+        .then((response)=> {
+          this.tvSeries = response.data.results
+          this.tvSeries.forEach((item, index) => {
+            // converto la base del voto da 10 a 5 usando una proporzione
+            let votoBase5 = Math.round(parseFloat(item.vote_average)*0.5)
+            // pongo il vecchio voto in base 10 uguale al nuovo voto in pase 5 specificando l'indice perché è un array di oggetti
+            this.tvSeries[index].vote_average = votoBase5;
 
           });
-          this.results = this.searched;
-          this.searched = '';
           // this.movies = [];
 
         })
