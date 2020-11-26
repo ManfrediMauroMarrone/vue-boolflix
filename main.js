53f5ee2bd4024d101e79d4335196850fb9f49534
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-11-26 16:14:58 +0100

    aggiunto indicatori ricerca

--- main.js
+++ main.js
@@ -5,58 +5,69 @@ var app = new Vue({
       tvSeries: [],
       languages:['de', 'en', 'es', 'fr', 'it', 'ja', 'ko'],
       searched: '',
-      results: '',
+      searchResults:'',
+      searching: false,
+      // results: '',
 
 
     },
 
     methods: {
       search() {
-        axios
-        .get('https://api.themoviedb.org/3/search/movie' ,{
-          params:{
-            api_key: '469d777c4caf641af43971c56a9b9340',
-            query: this.searched
-          }
-        })
-        .then((response)=> {
-          this.movies = response.data.results
-          this.movies.forEach((item, index) => {
-            // converto la base del voto da 10 a 5 usando una proporzione
-            let votoBase5 = Math.ceil(parseFloat(item.vote_average)*0.5)
-            // pongo il vecchio voto in base 10 uguale al nuovo voto in pase 5 specificando l'indice perché è un array di oggetti
-            this.movies[index].vote_average = votoBase5;
+        if (this.searched.trim() != "") {
 
+          this.searching = true;
+
+
+          axios
+          .get('https://api.themoviedb.org/3/search/movie' ,{
+            params:{
+              api_key: '469d777c4caf641af43971c56a9b9340',
+              query: this.searched
+            }
+          })
+          .then((response)=> {
+            this.movies = response.data.results
+            this.movies.forEach((item, index) => {
+              // converto la base del voto da 10 a 5 usando una proporzione
+              let votoBase5 = Math.ceil(parseFloat(item.vote_average)*0.5)
+              // pongo il vecchio voto in base 10 uguale al nuovo voto in pase 5 specificando l'indice perché è un array di oggetti
+              this.movies[index].vote_average = votoBase5;
+
+            });
+            this.searching = false
+            let searchedItem = this.searched;
+            this.searchResults = searchedItem;
+            this.searched = "";
+
+          })
+          .catch((error) =>{
+            console.log(error);
           });
 
+          axios
+          .get('https://api.themoviedb.org/3/search/tv' ,{
+            params:{
+              api_key: '469d777c4caf641af43971c56a9b9340',
+              query: this.searched
+            }
+          })
+          .then((response)=> {
+            this.tvSeries = response.data.results
+            this.tvSeries.forEach((item, index) => {
+              // converto la base del voto da 10 a 5 usando una proporzione
+              let votoBase5 = Math.round(parseFloat(item.vote_average)*0.5)
+              // pongo il vecchio voto in base 10 uguale al nuovo voto in pase 5 specificando l'indice perché è un array di oggetti
+              this.tvSeries[index].vote_average = votoBase5;
 
-        })
-        .catch((error) =>{
-          console.log(error);
-        });
+            });
+            // this.movies = [];
 
-        axios
-        .get('https://api.themoviedb.org/3/search/tv' ,{
-          params:{
-            api_key: '469d777c4caf641af43971c56a9b9340',
-            query: this.searched
-          }
-        })
-        .then((response)=> {
-          this.tvSeries = response.data.results
-          this.tvSeries.forEach((item, index) => {
-            // converto la base del voto da 10 a 5 usando una proporzione
-            let votoBase5 = Math.round(parseFloat(item.vote_average)*0.5)
-            // pongo il vecchio voto in base 10 uguale al nuovo voto in pase 5 specificando l'indice perché è un array di oggetti
-            this.tvSeries[index].vote_average = votoBase5;
-
+          })
+          .catch((error) =>{
+            console.log(error);
           });
-          // this.movies = [];
-
-        })
-        .catch((error) =>{
-          console.log(error);
-        });
+        }
 
       }
 
